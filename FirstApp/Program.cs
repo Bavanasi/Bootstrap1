fffad501c9d1d3d56724e4a517e07c588b9d495e
Bavanasi <bavanasi_vinitha@columbusstate.edu>
2023-03-30 20:46:30 +0000

    New blazor app

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -2,12 +2,16 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using FirstApp;
 using FirstApp.Clients;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddScoped<CustomAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(provider => provider.GetRequiredService<CustomAuthenticationStateProvider>());
+builder.Services.AddAuthorizationCore();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddHttpClient<NasaClient>();
+
 await builder.Build().RunAsync();
